2dd2b07d94e42934ef3861357212eabb58112f25
Alan H. Diniz <alanhdiniz@gmail.com>
2021-05-03 20:31:58 -0300

    Code is done

--- main.py
+++ main.py
@@ -28,10 +28,12 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(is_poisoning="S"))
     def return_baiacu(self):
         print("O animal é um baiacu.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_poisoning="N"))
     def return_atum(self):
         print("O animal é um atum.")
+        self.declare(Fact(ask_to_repeat="S"))
 
     @Rule(Fact(is_boned="N"))
     def ask_flat(self):
@@ -40,10 +42,12 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(has_flat_body="S"))
     def return_arraia(self):
         print("O animal é uma arraia.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(has_flat_body="N"))
     def return_tubarao(self):
         print("O animal é um tubarão.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_fish="N"))
     def ask_reptile(self):
@@ -60,6 +64,7 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(is_scaled="S"))
     def return_camaleao(self):
         print("O animal é um camaleão.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_scaled="N"))
     def ask_shell(self):
@@ -68,14 +73,17 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(has_shell="S"))
     def return_tartaruga(self):
         print("O animal é uma tartaruga.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(has_shell="N"))
     def return_jacare(self):
         print("O animal é um jacaré.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(has_four_members="N"))
     def return_cobra(self):
-        print("O animal é uma cobra")
+        print("O animal é uma cobra.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_reptile="N"))
     def ask_mammal(self):
@@ -92,6 +100,7 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(is_falconiform="S"))
     def return_gaviao(self):
         print("O animal é um gavião.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_falconiform="N"))
     def ask_immovable_on_air(self):
@@ -100,14 +109,17 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(can_immovable_flying="S"))
     def return_beija_flor(self):
         print("O animal é um beija-flor.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(can_immovable_flying="N"))
     def return_gaivota(self):
         print("O animal é uma gaivota.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_flying="N"))
     def return_pinguim(self):
         print("O animal é um pinguim.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_mammal="S"))
     def ask_primal(self):
@@ -128,22 +140,40 @@ class AnimalClassifierEngine(KnowledgeEngine):
     @Rule(Fact(is_canine="N"))
     def return_urso(self):
         print("O animal é um urso.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_canine="S"))
     def return_cao(self):
         print("O animal é um cão.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_nocturnal="S"))
     def return_morcego(self):
         print("O animal é um morcego.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_aquatic="S"))
     def return_baleia(self):
         print("O animal é uma baleia.")
+        self.declare(Fact(ask_to_repeat="S"))
     
     @Rule(Fact(is_primal="S"))
     def return_humano(self):
         print("O animal é um humano.")
+        self.declare(Fact(ask_to_repeat="S"))
+    
+    @Rule(Fact(ask_to_repeat="S"))
+    def ask_to_repeat(self):
+        self.declare(Fact(can_repeat=input("Deseja identificar outro animal? ")))
+    
+    @Rule(Fact(can_repeat="S"))
+    def start_repeat(self):
+        self.reset()
+        self.run()
+    
+    @Rule(Fact(can_repeat="N"))
+    def end_run(self):
+        print("Tchau.")
 
 engine = AnimalClassifierEngine()
 engine.reset()
